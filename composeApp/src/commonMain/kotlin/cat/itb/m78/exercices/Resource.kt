495ae1804c6c81077a9be0236673d152aa62c43a
CristianSalaF <cristian.sala.7e8@itb.cat>
2025-01-14 12:51:16 +0100

    Resource Exercise, removed unused imports.

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/Resource.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/Resource.kt
@@ -3,18 +3,14 @@ package cat.itb.m78.exercices
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.key.Key.Companion.R
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import m78exercices.composeapp.generated.resources.Res
 import m78exercices.composeapp.generated.resources.generatedFace
-import org.jetbrains.compose.resources.Resource
 import org.jetbrains.compose.resources.painterResource
 
 @Composable

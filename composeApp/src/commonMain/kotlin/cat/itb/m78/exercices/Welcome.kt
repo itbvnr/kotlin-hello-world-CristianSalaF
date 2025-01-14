7bcdb6144b5231d48fcfe435ab37e1afe356996b
CristianSalaF <cristian.sala.7e8@itb.cat>
2025-01-14 09:47:49 +0100

    Welcome exercise, removed unused imports

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/Welcome.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/Welcome.kt
@@ -8,11 +8,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlin.math.PI
 
 @Composable
 fun PrintWelcome() {

f30d76212bfd6333b141ff396b0d0bf111703017
CristianSalaF <cristian.sala.7e8@itb.cat>
2025-01-16 09:28:53 +0100

    Contact exercise: fixed styling, almost 1:1. Lacks font type.

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/contact.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/contact.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import m78exercices.composeapp.generated.resources.Res
@@ -29,17 +30,17 @@ fun PrintContact() {
 
         Image(
             painter = painterResource(Res.drawable.generatedFace),
-            modifier = Modifier.size(128.dp).padding(20.dp).clip(CircleShape),
+            modifier = Modifier.size(128.dp).padding(10.dp).clip(CircleShape),
             contentDescription = null)
 
-        Text(contact.fullName, fontSize = 20.sp)
-        Spacer(modifier = Modifier.height(10.dp))
+        Text(contact.fullName, fontSize = 25.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(20.dp))
 
-        Box(modifier = Modifier.clip(RoundedCornerShape(percent = 40))/*.clip(CutCornerShape(percent = 50))*/.background(Color.LightGray).padding(10.dp)) {
-            Column(horizontalAlignment = Alignment.CenterHorizontally,
+        Box(modifier = Modifier.padding(10.dp).clip(RoundedCornerShape(percent = 10)).background(Color.LightGray).padding(20.dp)) {
+            Column(horizontalAlignment = Alignment.Start,
                 verticalArrangement = Arrangement.Center){
-                Text("✉ "+contact.email, fontSize = 10.sp)
-                Text("📞 "+contact.phone, fontSize = 10.sp)
+                Text("✉ "+contact.email, fontSize = 15.sp)
+                Text("📞 "+contact.phone, fontSize = 15.sp)
             }
         }
     }
